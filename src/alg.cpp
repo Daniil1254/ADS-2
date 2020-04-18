27079e701198bd5f45aa37efd30ef7fee344da6f
Daniil1254 <62605512+Daniil1254@users.noreply.github.com>
2020-04-18 15:31:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,28 @@
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>
 using namespace std;
+
+void sortAscending(int *arr, int len)
+{
+	int min;
+	int address;
+	int c;
+	for (int i = 0; i < len - 1; i++)
+	{
+		min = arr[i];
+		address = i;
+		for (int j = i + 1; j < len; j++)
+		if (arr[j] < min)
+		{
+			min = arr[j];
+			address = j;
+		}
+		c = arr[i];
+		arr[i] = min;
+		arr[address] = c;
+	}
+}
 
 int countPairs1(int *arr, int len, int value)
 {
@@ -14,15 +38,18 @@ int countPairs2(int *arr, int len, int value)
 {
 	int k = 0;
 	int rightBorder = len;
-	
+	sortAscending(arr, len);
 
 	for (int leftBorder = 0; leftBorder < rightBorder; leftBorder++)
 	for (int i = leftBorder + 1; i < rightBorder; i++)
-	{
-		if (arr[i] + arr[leftBorder] == value)
-			k++;
-		else if (arr[i] + arr[leftBorder] > value)
+	{	
+		if (arr[i] + arr[leftBorder] > value)
+		{
 			rightBorder = i;
+			break;
+		}
+		else if(arr[i] + arr[leftBorder] == value)
+			 k++;
 	}
 	return k;
 }
@@ -35,7 +62,7 @@ int countPairs3(int *arr, int len, int value)
 	int center;
 	int rightBorder = len;
 	int leftBorder = 0;
-	
+	sortAscending(arr, len);
 
 	for (int i = 0; ((i < len) && (leftBorder < rightBorder)); i++)
 	{
